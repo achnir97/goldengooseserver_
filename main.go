31e1486ff47a442e9e9c21de4185dbd630d0edd4
Vogo <nirmalvn@Vogos-MacBook-Pro.local>
2023-03-02 09:20:45 +0900

    signup and signin function added

--- main.go
+++ main.go
@@ -6,67 +6,150 @@ import (
 	"log"
 	"net/http"
 	"github.com/achnir97/server/config"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	
 )
 
 type User struct {
+	    gorm.Model 
 		FirstName string `json:"firstName"`
 		LastName  string `json:"lastName"`
 		Email     string `json:"email"`
 		Password  string `json:"password"`
+		Date 	 string `json:"date"`
+		NumberofGoose string `json:"numberofgoose"`
+
 	}
 
 func main() {
-
+	
 	http.HandleFunc("/register", register)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/calculate/{id}",Calculate)
+	corsHandler := corsMiddleware(http.DefaultServeMux)
+	log.Fatal(http.ListenAndServe(":8080", corsHandler))	
 }
-func register(w http.ResponseWriter, r *http.Request) {
-	db, err:=config.Connect()
 
+func register(w http.ResponseWriter, r *http.Request) {
+    db, err := config.Connect() // connects to the database
+    migrator := db.Migrator() // check if the database exist or not 
+
+    if migrator.HasTable(&User{}) { // first checks if the table exist or not 
+        fmt.Println("Table 'users' already exists") 
+    } else {
+        // Create the users table
+        err := db.AutoMigrate(&User{})
+        if err != nil {
+            panic(err)
+        }
+        fmt.Println("Table 'users' created successfully")
+    }
+    var user User
+    err = json.NewDecoder(r.Body).Decode(&user)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    // Hash the password
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Store the hashed password in the database
+    db.Exec("INSERT INTO users (first_name, last_name, email, password,date, numberof_goose) VALUES ($1, $2, $3, $4, $5,$6)", user.FirstName, user.LastName, user.Email, string(hashedPassword),user.Date, user.NumberofGoose)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "User %s %s created successfully!", user.FirstName, user.LastName)
+}
+
+
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+	// Connect to the database
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Parse the request body
 	var user User
 	err = json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	fmt.Println(user.Email)
+	fmt.Println(user.Password)
 
-	db.Exec("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)", user.FirstName, user.LastName, user.Email, user.Password)
+	// Check if user exists in the database
+	var existingUser User
+	db.Where("email = ?", user.Email).First(&existingUser)
+	if existingUser.Email == "" {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		fmt.Println("the email is empty")
+		return
+	}
+
+	// Compare the passwords
+	  if err := bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(user.Password)); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		fmt.Printf("existingUser.Password:  %s\n", []byte(existingUser.Password))
+		fmt.Printf("Error is %s\n", err)
+		return
+	}
+	// Successful login
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "You are now logged in!")
+}
+
+
+func Calculate(w http.ResponseWriter, r *http.Request) {
+	// Parse the user ID from the request parameters
+	id := r.URL.Query().Get("id")
+	fmt.Printf(id)
+	// Connect to the database
+	db, err := config.Connect()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+   // Execute the raw SQL query
+	var numberofgoose int
+	data := db.Raw("SELECT numberof_goose FROM users WHERE id = ?", 1).Scan(&numberofgoose)
+	if data.Error != nil {
+		http.Error(w, data.Error.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "User %s %s created successfully!", user.FirstName, user.LastName)
+	// Send the response back to the client
+	response := map[string]int{"gooseCount": numberofgoose}
+	json.NewEncoder(w).Encode(response)
 }
 
 
+func corsMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-/*
-func signupHandler(db *gorm.DB) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request) {
-		var user User
-		err := json.NewDecoder(r.Body).Decode(&user)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+		// Handle preflight requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
 			return
 		}
-
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		user.Password = string(hashedPassword)
-		err = db.Create(&user).Error
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(map[string]int{"id": int(user.ID)})
-	}
-}*/
+		// Call the next handler
+		h.ServeHTTP(w, r)
+	})
+}
 
